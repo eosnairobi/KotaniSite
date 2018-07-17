fa909795e308416f757e33e0234e2949990624b9
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2018-07-17 14:33:29 +0300

    [Feature #159102194] Implemented nav area with routes to diff areas.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/layout/Header";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Route exact path="/" component={Header} />
+        </div>
+      </Router>
+    );
   }
 }
 
