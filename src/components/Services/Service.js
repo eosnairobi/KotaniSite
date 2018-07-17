482267cb5510aa8f70137b32a6e6edb3a2e66064
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2018-07-17 18:59:02 +0300

    [Bug #159109489] Image animation resolved.

--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Carousel from "nuka-carousel";
 
 import "./Service.css";
 
@@ -8,69 +9,76 @@ class Service extends Component {
     return (
       <section className="mosh-service-area clearfix">
         <div className="container">
-          <div className="row">
+          <div className="row" style={{ marginBottom: "1rem" }}>
             <div className="col-12">
-              <div className="mosh-service-slides owl-carousel">
-                {/* <!-- Single Service Area --> */}
-                <div className="single-service-area text-center">
-                  <h2>01.</h2>
-                  <h4>No1 in Sales!</h4>
-                  <p>
-                    Etiam nec odio vestibulum est mattis effic iturut magna.
-                    Pellentesque sit am et tellus blandit. Etiam nec odio
-                    vestibul.
-                  </p>
-                </div>
-                {/* <!-- Single Service Area --> */}
-                <div className="single-service-area text-center">
-                  <h2>02.</h2>
-                  <h4>Online Marketing</h4>
-                  <p>
-                    Etiam nec odio vestibulum est mattis effic iturut magna.
-                    Pellentesque sit am et tellus blandit. Etiam nec odio
-                    vestibul.
-                  </p>
-                </div>
-                {/* <!-- Single Service Area --> */}
-                <div className="single-service-area text-center">
-                  <h2>03.</h2>
-                  <h4>Modern Design</h4>
-                  <p>
-                    Etiam nec odio vestibulum est mattis effic iturut magna.
-                    Pellentesque sit am et tellus blandit. Etiam nec odio
-                    vestibul.
-                  </p>
-                </div>
-                {/* <!-- Single Service Area --> */}
-                <div className="single-service-area text-center">
-                  <h2>04.</h2>
-                  <h4>No1 in Sales!</h4>
-                  <p>
-                    Etiam nec odio vestibulum est mattis effic iturut magna.
-                    Pellentesque sit am et tellus blandit. Etiam nec odio
-                    vestibul.
-                  </p>
-                </div>
-                {/* <!-- Single Service Area --> */}
-                <div className="single-service-area text-center">
-                  <h2>05.</h2>
-                  <h4>Online Marketing</h4>
-                  <p>
-                    Etiam nec odio vestibulum est mattis effic iturut magna.
-                    Pellentesque sit am et tellus blandit. Etiam nec odio
-                    vestibul.
-                  </p>
-                </div>
-                {/* <!-- Single Service Area --> */}
-                <div className="single-service-area text-center">
-                  <h2>06.</h2>
-                  <h4>Modern Design</h4>
-                  <p>
-                    Etiam nec odio vestibulum est mattis effic iturut magna.
-                    Pellentesque sit am et tellus blandit. Etiam nec odio
-                    vestibul.
-                  </p>
-                </div>
+              <div className="mosh-service-slides">
+                <Carousel
+                  dragging={true}
+                  autoplay={true}
+                  slideWidth={0.33}
+                  cellAlign="center"
+                >
+                  {/* <!-- Single Service Area --> */}
+                  <div className="single-service-area text-center">
+                    <h2>01.</h2>
+                    <h4>No1 in Sales!</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mattis effic iturut magna.
+                      Pellentesque sit am et tellus blandit. Etiam nec odio
+                      vestibul.
+                    </p>
+                  </div>
+                  {/* <!-- Single Service Area --> */}
+                  <div className="single-service-area text-center">
+                    <h2>02.</h2>
+                    <h4>Online Marketing</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mattis effic iturut magna.
+                      Pellentesque sit am et tellus blandit. Etiam nec odio
+                      vestibul.
+                    </p>
+                  </div>
+                  {/* <!-- Single Service Area --> */}
+                  <div className="single-service-area text-center">
+                    <h2>03.</h2>
+                    <h4>Modern Design</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mattis effic iturut magna.
+                      Pellentesque sit am et tellus blandit. Etiam nec odio
+                      vestibul.
+                    </p>
+                  </div>
+                  {/* <!-- Single Service Area --> */}
+                  <div className="single-service-area text-center">
+                    <h2>04.</h2>
+                    <h4>No1 in Sales!</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mattis effic iturut magna.
+                      Pellentesque sit am et tellus blandit. Etiam nec odio
+                      vestibul.
+                    </p>
+                  </div>
+                  {/* <!-- Single Service Area --> */}
+                  <div className="single-service-area text-center">
+                    <h2>05.</h2>
+                    <h4>Online Marketing</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mattis effic iturut magna.
+                      Pellentesque sit am et tellus blandit. Etiam nec odio
+                      vestibul.
+                    </p>
+                  </div>
+                  {/* <!-- Single Service Area --> */}
+                  <div className="single-service-area text-center">
+                    <h2>06.</h2>
+                    <h4>Modern Design</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mattis effic iturut magna.
+                      Pellentesque sit am et tellus blandit. Etiam nec odio
+                      vestibul.
+                    </p>
+                  </div>
+                </Carousel>
               </div>
             </div>
             {/* <!-- Discover More btn --> */}
