68a404213ecae8b371ef850001a474353f4d6434
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2018-07-17 17:13:55 +0300

    [Feature #159107524] Integration of breadcrumb header section

---
+++ src/components/Services/Servicepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Breadcrumb from "../common/BreadCrumb";
+
+class Servicepage extends Component {
+  render() {
+    return (
+      <div>
+        <Breadcrumb title="Services" description="What we offer." />
+      </div>
+    );
+  }
+}
+
+export default Servicepage;
