b0cf7a61bd2584209ddcbab3acf18eddcacceaf6
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2018-07-17 17:30:56 +0300

    [Feature #159107524] Added a Service skill section, feature section and a subscription form

--- src/components/Services/Servicepage.js
+++ src/components/Services/Servicepage.js
@@ -1,11 +1,156 @@
 import React, { Component } from "react";
 import Breadcrumb from "../common/BreadCrumb";
+import Subscription from "../CTA/Subscription";
+import SkillArea from "../skills/SkillsArea";
 
 class Servicepage extends Component {
   render() {
     return (
       <div>
         <Breadcrumb title="Services" description="What we offer." />
+        <section class="mosh--services-area section_padding_100">
+          <div class="container">
+            <div class="row">
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/trophy.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>No1 in Sales!</h4>
+                    <p>
+                      Nec odio vestibulum est mattis effic iturut magna.
+                      Pellentesque sit am.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/edit.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>Documentation inside</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/video-camera.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>Modern Design</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/presentation.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>Marketing Strategist</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/gamepad.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>Play with colors</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/atomic.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>Mix &amp; Match</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/safebox.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>Worth the money</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/settings-2.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>Modern elements</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Single Feature Area --> */}
+              <div class="col-12 col-sm-6 col-md-4">
+                <div class="single-feature-area d-flex mb-100">
+                  <div class="feature-icon mr-30">
+                    <img src="img/core-img/alarm-clock.png" alt="" />
+                  </div>
+                  <div class="feature-content">
+                    <h4>24 Hours support</h4>
+                    <p>
+                      Etiam nec odio vestibulum est mat tis effic iturut magna.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-12 text-center">
+                <a href="#" class="btn mosh-btn">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <SkillArea />
+        <Subscription />
       </div>
     );
   }
---
+++ src/components/skills/SkillsArea.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+const SkillArea = () => {
+  return (
+    <section class="service-skills-area section_padding_100">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <div class="row">
+              <div class="col-6">
+                <div
+                  class="single-pie-bar text-center mb-100"
+                  data-percent="75"
+                >
+                  <canvas class="bar-circle" width="70" height="70" />
+                  <h6>Awesome</h6>
+                  <p>Etiam nec odio</p>
+                </div>
+              </div>
+              <div class="col-6">
+                <div
+                  class="single-pie-bar text-center mb-100"
+                  data-percent="83"
+                >
+                  <canvas class="bar-circle" width="70" height="70" />
+                  <h6>Creative</h6>
+                  <p>Etiam nec odio</p>
+                </div>
+              </div>
+              <div class="col-6">
+                <div
+                  class="single-pie-bar text-center mb-100"
+                  data-percent="25"
+                >
+                  <canvas class="bar-circle" width="70" height="70" />
+                  <h6>Lazy</h6>
+                  <p>Etiam nec odio</p>
+                </div>
+              </div>
+              <div class="col-6">
+                <div
+                  class="single-pie-bar text-center mb-100"
+                  data-percent="75"
+                >
+                  <canvas class="bar-circle" width="70" height="70" />
+                  <h6>Outstanding</h6>
+                  <p>Etiam nec odio</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6">
+            <div class="service-skills-content">
+              <div class="section-heading">
+                <p>Features</p>
+                <h2>What you get if you start your business with us</h2>
+              </div>
+              <div class="skills-text d-flex">
+                <p>
+                  Etiam nec odio vestibulum est mattis effic iturut magna.
+                  Pellentesque sit am et tellus blandit. Etiam nec odio
+                  vestibul. Etiam nec odio vestibulum est mat tis effic iturut
+                  magna. Pellentesque sit amet tellus blandit. Etiam nec odio
+                  ves tibul. Nec odio vestibulum est mattis effic iturut magna.
+                  Pellente sque sita met tellus blandit.
+                </p>
+                <p>
+                  Pellentesque sit am et tellus blandit. Etiam nec odio
+                  vestibul. Etiam nec odio vestibulum est mat tis effic iturut
+                  magna. Pellentesque sit amet tellus blandit. Etiam nec odio
+                  ves tibul. Nec odio vestibulum est mattis effic iturut magna.
+                  Pellente sque sita met tellus blandit. Etiam nec odio. Etiam
+                  nec odio vestibulum est mattis effic.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SkillArea;
