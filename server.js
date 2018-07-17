9fce57237e0a5183e7bd20cd02037164de4e3537
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2018-07-17 20:26:17 +0300

    [Deploy] fixed deployement route issues

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Use Routes
+app.use("/api/users", users);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
